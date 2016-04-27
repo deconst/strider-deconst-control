99f9bd30232c17ada72b1c3ccea191c460f7d2fd
Ash Wilson <smashwilson@gmail.com>
2016-04-27 10:05:48 -0400

    Massive formatting-only diff

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
-var Toolbelt = require('strider-deconst-common').Toolbelt;
-var entry = require('./lib/entry');
+var Toolbelt = require('strider-deconst-common').Toolbelt
+var entry = require('./lib/entry')
 
 exports.init = function (config, job, jobContext, callback) {
   callback(null, {
@@ -7,26 +7,26 @@ exports.init = function (config, job, jobContext, callback) {
     path: [],
 
     deploy: function (phaseContext, cb) {
-      var toolbelt = new Toolbelt(config, job, jobContext, phaseContext);
+      var toolbelt = new Toolbelt(config, job, jobContext, phaseContext)
 
-      if (hadError(toolbelt.connectToDocker(), cb)) return;
-      if (hadError(toolbelt.connectToGitHub(), cb)) return;
-      if (hadError(toolbelt.connectToContentService(true), cb)) return;
+      if (hadError(toolbelt.connectToDocker(), cb)) return
+      if (hadError(toolbelt.connectToGitHub(), cb)) return
+      if (hadError(toolbelt.connectToContentService(true), cb)) return
 
       entry.prepareControlRepository(toolbelt, function (err) {
-        if (hadError(err, cb)) return;
-        cb(null, true);
-      });
+        if (hadError(err, cb)) return
+        cb(null, true)
+      })
     }
-  });
-};
+  })
+}
 
 var hadError = function (err, callback) {
   if (err) {
-    err.type = 'exitCode';
-    err.code = 1;
+    err.type = 'exitCode'
+    err.code = 1
 
-    if (callback) callback(err);
+    if (callback) callback(err)
     return true
   }
 
